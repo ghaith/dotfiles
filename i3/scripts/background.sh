ee2d295fdd3b2db010b52feeb08671ff4ad230ed
Ghaith Hachem <ghaith.hachem@gmail.com>
2022-01-10 04:56:53 +0100

    remove lsp signature, add lualine, change wallpaper

--- i3/scripts/background.sh
+++
@@ -1,1 +0,0 @@
-/home/ghaith/.config/background.sh
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -94,11 +94,6 @@ _G.packer_plugins = {
     path = "/home/ghaith/.local/share/nvim/site/pack/packer/start/lsp-status.nvim",
     url = "https://github.com/nvim-lua/lsp-status.nvim"
   },
-  ["lsp_signature.nvim"] = {
-    loaded = true,
-    path = "/home/ghaith/.local/share/nvim/site/pack/packer/start/lsp_signature.nvim",
-    url = "https://github.com/ray-x/lsp_signature.nvim"
-  },
   ["lualine.nvim"] = {
     loaded = true,
     path = "/home/ghaith/.local/share/nvim/site/pack/packer/start/lualine.nvim",
