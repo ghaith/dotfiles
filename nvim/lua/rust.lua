651e203883100888c6dc752765e94ed4f9238053
Ghaith Hachem <ghaith.hachem@gmail.com>
2022-08-31 14:09:06 +0200

    Add rust check options to cargo analyzer

--- nvim/lua/rust.lua
+++ nvim/lua/rust.lua
@@ -71,6 +71,7 @@ local extension_path = vim.env.HOME .. '/.local/codelldb/'
 local codelldb_path = extension_path .. 'adapter/codelldb'
 local liblldb_path = extension_path .. 'lldb/lib/liblldb.so'
 
+
 local opts = {
 	tools = {
 		-- whether to show variable name before type hints with the inlay hints or not
@@ -81,10 +82,39 @@ local opts = {
 		on_attach = on_attach ,
 		capabilities = lsp.capabilities,
 		["rust-analyzer"] = {
+			on_init = function(client)
+				-- Override rust command if within the rust repo. hardcoded for now
+				local path = client.workspace_folders[1].name
+				if path:sub(-#"/rust") == "/rust" then
+						client.config.settings["rust-analyzer"].checkOnSave.overrideCommand = { 
+							"python3", 
+							"x.py", 
+							"check", 
+							"--json-output" ,
+							"--build-dir",
+							"build-rust-analyzer"
+						}
+						client.config.settings["rust-analyzer"].procMacro.enable = true
+						client.config.settings["rust-analyzer"].cargo.buildScripts.enable = true
+						client.config.settings["rust-analyzer"].cargo.buildScripts.overrideCommand = {
+										"python3",
+										"x.py",
+										"check",
+										"--json-output",
+										"--build-dir",
+										"build-rust-analyzer"
+						}
+						client.config.settings["rust-analyzer"].rustc.source = "./Cargo.toml"
+				end
+			end,
 			--enable clippy on save
 			checkOnSave = {
-				command = "clippy",
+				-- command = "clippy",
+				overrideCommand = {"cargo", "check", "--build-dir", "build-rust-analyzer" }
 			},
+			buildScript = {
+				overrideCommand = {"cargo", "check", "--build-dir", "build-rust-analyzer" }
+			}
 		},
 	},
 	dap = {
