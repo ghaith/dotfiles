67293b153d07454c5912f9a3224e8e4163b8d87b
Ghaith Hachem <ghaith.hachem@gmail.com>
2023-08-04 22:58:03 +0200

    Initial nixos config

---
+++ nixos/configuration.nix
@@ -0,0 +1,176 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "ghaith-thinkpad"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Vienna";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_AT.UTF-8";
+    LC_IDENTIFICATION = "de_AT.UTF-8";
+    LC_MEASUREMENT = "de_AT.UTF-8";
+    LC_MONETARY = "de_AT.UTF-8";
+    LC_NAME = "de_AT.UTF-8";
+    LC_NUMERIC = "de_AT.UTF-8";
+    LC_PAPER = "de_AT.UTF-8";
+    LC_TELEPHONE = "de_AT.UTF-8";
+    LC_TIME = "de_AT.UTF-8";
+  };
+
+  # Enable nvidia
+  # Make sure opengl is enabled
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Tell Xorg to use the nvidia driver
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is needed for most wayland compositors
+    modesetting.enable = true;
+
+    # Use the open source version of the kernel module
+    # Only available on driver 515.43.04+
+    open = true;
+
+    # Enable the nvidia settings menu
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+  
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "at";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ghaith = {
+    isNormalUser = true;
+    description = "Ghaith Hachem";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+    #  thunderbird
+    ];
+  };
+
+  # Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "ghaith";
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+     helix
+     vim 
+     git
+     curl
+     wget
+
+  ];
+
+  # Enable experimental commands
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+     enable = true;
+     enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  services.fwupd.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable flatpak
+  services.flatpak.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
---
+++ nixos/flake.nix
@@ -0,0 +1,89 @@
+{
+  description = "Ghaith's NixOS Flake";
+
+  # Inputs
+  # https://nixos.org/manual/nix/unstable/command-ref/new-cli/nix3-flake.html#flake-inputs
+
+  # The flake in the current directory.
+  # inputs.currentDir.url = ".";
+
+  # A flake in some other directory.
+  # inputs.otherDir.url = "/home/alice/src/patchelf";
+
+  # A flake in some absolute path
+  # inputs.otherDir.url = "path:/home/alice/src/patchelf";
+
+  inputs = {
+    # Official sources
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # home-manager, used for user config
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+# `outputs` are all the build result of the flake.
+  # A flake can have many use cases and different types of outputs.
+  # parameters in `outputs` are defined in `inputs` and can be referenced by their names.
+  # However, `self` is an exception, This special parameter points to the `outputs` itself (self-reference)
+  # The `@` syntax here is used to alias the attribute set of the inputs's parameter, making it convenient to use inside the function.
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      # By default, NixOS will try to refer the nixosConfiguration with its hostname.
+      # so the system named `nixos-test` will use this configuration.
+      # However, the configuration name can also be specified using `sudo nixos-rebuild switch --flake /path/to/flakes/directory#<name>`.
+      # The `nixpkgs.lib.nixosSystem` function is used to build this configuration, the following attribute set is its parameter.
+      # Run `sudo nixos-rebuild switch --flake .#nixos-test` in the flake's directory to deploy this configuration on any NixOS system
+      "ghaith-thinkpad" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        # The Nix module system can modularize configuration, improving the maintainability of configuration.
+        #
+        # Each parameter in the `modules` is a Nix Module, and there is a partial introduction to it in the nixpkgs manual:
+        #    <https://nixos.org/manual/nixpkgs/unstable/#module-system-introduction>
+        # It is said to be partial because the documentation is not complete, only some simple introductions
+        #    (such is the current state of Nix documentation...)
+        # A Nix Module can be an attribute set, or a function that returns an attribute set.
+        # If a Module is a function, this function can only have the following parameters:
+        #
+        #  lib:     the nixpkgs function library, which provides many useful functions for operating Nix expressions
+        #            https://nixos.org/manual/nixpkgs/stable/#id-1.4
+        #  config:  all config options of the current flake
+        #  options: all options defined in all NixOS Modules in the current flake
+        #  pkgs:   a collection of all packages defined in nixpkgs.
+        #           you can assume its default value is `nixpkgs.legacyPackages."${system}"` for now.
+        #           can be customed by `nixpkgs.pkgs` option
+        #  modulesPath: the default path of nixpkgs's builtin modules folder,
+        #               used to import some extra modules from nixpkgs.
+        #               this parameter is rarely used, you can ignore it for now.
+        #
+        # Only these parameters can be passed by default.
+        # If you need to pass other parameters, you must use `specialArgs` by uncomment the following line
+        # specialArgs = {...}  # pass custom arguments into sub module.
+	specialArgs = inputs;
+        modules = [
+          # Import the configuration.nix we used before, so that the old configuration file can still take effect.
+          # Note: /etc/nixos/configuration.nix itself is also a Nix Module, so you can import it directly here
+          ./configuration.nix
+
+	 # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.ghaith = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
+      };
+    };
+  };
+}
