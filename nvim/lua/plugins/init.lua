6362c5439a8b069346a0f58a055bcc9deae7fc5d
Ghaith Hachem <ghaith.hachem@gmail.com>
2024-04-12 22:37:16 +0200

    Restore brainiac as main pc, update nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -72,7 +72,8 @@ require("lazy").setup({
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"antoinemadec/FixCursorHold.nvim",
-			"nvim-treesitter/nvim-treesitter"
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-neotest/nvim-nio"
 		},
 	},
 
