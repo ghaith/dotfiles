68d12289a7fbe4214dcdec862ab7988a1ed9d97f
Ghaith Hachem <ghaith@ghaith-xps.kpn>
2024-09-05 21:35:23 +0200

    add homebrew, fix a typo and update telescope

--- nvim/lua/telescope_config.lua
+++ nvim/lua/telescope_config.lua
@@ -1,5 +1,5 @@
 local actions = require("telescope.actions")
-local trouble = require("trouble.providers.telescope")
+local trouble = require("trouble.sources.telescope")
 local telescope = require("telescope")
 
 require('telescope').load_extension('fzf')
