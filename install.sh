9ff9de4b01832a622bbeaee0a52d1d9f10c44ff4
Ghaith Hachem <ghaith.hachem@gmail.com>
2024-10-24 08:44:44 +0200

    Add dotter dev container and point dotter to it

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 # If dotter is not yet installed, download dotter
 if ! which dotter > /dev/null; then
-	VER=$(curl --silent -qI https://github.com/SuperCuber/dotter/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}')\;
+	VER=$(curl --silent -qI https://github.com/SuperCuber/dotter/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}')
 	wget https://github.com/SuperCuber/dotter/releases/download/$VER/dotter-linux-x64-musl -O dotter
 	chmod +x ./dotter
 fi
 
 # run dotter
 if ! which dotter > /dev/null; then
-	./dotter
+	./dotter -l .dotter/devcontainer.toml
 else
 	dotter
 fi
