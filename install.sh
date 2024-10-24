38f43759c636f3ebe6f95a71454dba17d2eaf627
Ghaith Hachem <ghaith.hachem@gmail.com>
2024-10-24 08:37:27 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# If dotter is not yet installed, download dotter
+if ! which dotter > /dev/null; then
+	VER=$(curl --silent -qI https://github.com/SuperCuber/dotter/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}')\;
+	wget https://github.com/SuperCuber/dotter/releases/download/$VER/dotter-linux-x64-musl -O dotter
+	chmod +x ./dotter
+fi
+
+# run dotter
+if ! which dotter > /dev/null; then
+	./dotter
+else
+	dotter
+fi
