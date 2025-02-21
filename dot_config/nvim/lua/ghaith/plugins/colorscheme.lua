793aad9bff24f24c600432b99f3dc92129eaeb1d
Ghaith Hachem <git@mail.hachem.at>
2025-02-21 06:20:54 +0100

    update zellij config

--- dot_config/nvim/lua/ghaith/plugins/colorscheme.lua
+++ dot_config/nvim/lua/ghaith/plugins/colorscheme.lua
@@ -5,16 +5,16 @@ return {
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'catppuccin/nvim',
-    name = "catppuccin",
+    name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme 'catppuccin-latte'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
 }
