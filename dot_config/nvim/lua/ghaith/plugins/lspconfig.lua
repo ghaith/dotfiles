905bca5fa50b4e508687ecddd9818e73051cc7c0
Ghaith Hachem <git@mail.hachem.at>
2025-02-28 06:13:04 +0100

    vim shortcut - diagnostics

--- dot_config/nvim/lua/ghaith/plugins/lspconfig.lua
+++ dot_config/nvim/lua/ghaith/plugins/lspconfig.lua
@@ -110,6 +110,9 @@ return {
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          -- Show the diagnostic for the current line
+          map('<leader>ld', vim.diagnostic.open_float, '[L]ine [D]iagnostics')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
