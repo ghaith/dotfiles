33535d6e1c1aa9c17e691efe82b775b0901a5733
Ghaith Hachem <git@mail.hachem.at>
2025-03-12 10:31:23 +0100

    remove conflict between stepping and symbols

--- dot_config/nvim/lua/ghaith/plugins/lspconfig.lua
+++ dot_config/nvim/lua/ghaith/plugins/lspconfig.lua
@@ -92,7 +92,7 @@ return {
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+          map('<leader>lds', require('telescope.builtin').lsp_document_symbols, '[L]SP [D]ocument [S]ymbols')
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
