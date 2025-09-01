089b73b0252df2917a0d23ea3d972397a5fa189b
Ghaith Hachem <git@mail.hachem.at>
2025-09-01 08:45:00 +0200

    disable copilot custom config

--- dot_config/nvim/lua/ghaith/plugins/copilot.lua
+++ dot_config/nvim/lua/ghaith/plugins/copilot.lua
@@ -2,26 +2,26 @@ return {
   'github/copilot.vim',
   config = function()
     -- Disable default ghost text (since we're using cmp integration)
-    vim.g.copilot_no_tab_map = true
-    vim.g.copilot_assume_mapped = true
-    
-    -- Optional: Disable Copilot by default, enable on-demand
-    -- vim.g.copilot_enabled = false
-    
-    -- Keymaps for manual control
-    vim.keymap.set('n', '<leader>ce', '<cmd>Copilot enable<CR>', { desc = 'Enable Copilot' })
-    vim.keymap.set('n', '<leader>cd', '<cmd>Copilot disable<CR>', { desc = 'Disable Copilot' })
-    vim.keymap.set('n', '<leader>cs', '<cmd>Copilot status<CR>', { desc = 'Copilot Status' })
-    
-    -- Toggle Copilot on/off
-    vim.keymap.set('n', '<leader>ct', function()
-      if vim.g.copilot_enabled == false then
-        vim.cmd('Copilot enable')
-        print('Copilot enabled')
-      else
-        vim.cmd('Copilot disable')
-        print('Copilot disabled')
-      end
-    end, { desc = 'Toggle Copilot' })
+    -- vim.g.copilot_no_tab_map = true
+    -- vim.g.copilot_assume_mapped = true
+    --
+    -- -- Optional: Disable Copilot by default, enable on-demand
+    -- -- vim.g.copilot_enabled = false
+    --
+    -- -- Keymaps for manual control
+    -- vim.keymap.set('n', '<leader>ce', '<cmd>Copilot enable<CR>', { desc = 'Enable Copilot' })
+    -- vim.keymap.set('n', '<leader>cd', '<cmd>Copilot disable<CR>', { desc = 'Disable Copilot' })
+    -- vim.keymap.set('n', '<leader>cs', '<cmd>Copilot status<CR>', { desc = 'Copilot Status' })
+    --
+    -- -- Toggle Copilot on/off
+    -- vim.keymap.set('n', '<leader>ct', function()
+    --   if vim.g.copilot_enabled == false then
+    --     vim.cmd('Copilot enable')
+    --     print('Copilot enabled')
+    --   else
+    --     vim.cmd('Copilot disable')
+    --     print('Copilot disabled')
+    --   end
+    -- end, { desc = 'Toggle Copilot' })
   end,
 }
