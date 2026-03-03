4cbad93018d31370c668f2005539ea5ea7d5359a
Ghaith Hachem <git@mail.hachem.at>
2026-03-03 20:36:57 +0100

    dark themes

--- dot_config/nvim/lua/ghaith/plugins/colorscheme.lua
+++ dot_config/nvim/lua/ghaith/plugins/colorscheme.lua
@@ -11,7 +11,7 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-latte'
+      vim.cmd.colorscheme 'catppuccin-mocha'
 
       -- You can configure highlights by doing something like:
       -- vim.cmd.hi 'Comment gui=none'
