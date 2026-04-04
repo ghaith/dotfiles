62f00f299de8bbc28c13286ab55bbb90fe253eb5
Ghaith Hachem <git@mail.hachem.at>
2026-04-04 12:08:40 +0200

    home manager hm-backup

--- nixos/modules/nixos.nix
+++ nixos/modules/nixos.nix
@@ -8,6 +8,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "hm-backup";
           home-manager.extraSpecialArgs = { inherit inputs self; };
         }
       ];
